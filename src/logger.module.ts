7801b72825628498f566aa3ffd1a76fad8a11987
Matvey Alekseev <mat@twg.su>
2025-07-26 17:56:23 +0300

    update

--- src/logger.module.ts
+++ src/logger.module.ts
@@ -2,18 +2,24 @@ import { Module, DynamicModule, Provider, Global } from '@nestjs/common';
 import { Logger } from './logger';
 import { LoggerModuleOptions } from './interfaces';
 
+export const LOGGER_MODULE_OPTIONS = 'LOGGER_MODULE_OPTIONS';
+
 @Global()
 @Module({})
 export class LoggerModule {
   static forRoot(options: LoggerModuleOptions = {}): DynamicModule {
+    const optionsProvider: Provider = {
+      provide: LOGGER_MODULE_OPTIONS,
+      useValue: options,
+    };
     const loggerProvider: Provider = {
       provide: Logger,
-      useValue: new Logger(options.context, options.loggerOptions),
+      useClass: Logger,
     };
     return {
       module: LoggerModule,
-      providers: [loggerProvider],
-      exports: [loggerProvider],
+      providers: [optionsProvider, loggerProvider],
+      exports: [Logger],
     };
   }
 }
